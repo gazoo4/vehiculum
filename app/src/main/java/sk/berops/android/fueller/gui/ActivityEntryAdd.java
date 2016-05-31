d758242cde93b5ff3d085c9835de429f2c39157f
gazoo4 <bernard.halas@gmail.com>
2016-05-31 19:30:30 +0200

    added refuel icon added tyres change icon

--- app/src/main/java/sk/berops/android/fueller/gui/ActivityEntryAdd.java
+++ app/src/main/java/sk/berops/android/fueller/gui/ActivityEntryAdd.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import sk.berops.android.fueller.R;
 import sk.berops.android.fueller.gui.burreaucratic.ActivityBurreaucraticAdd;
@@ -15,11 +16,28 @@ import sk.berops.android.fueller.gui.service.ActivityServiceAdd;
 import sk.berops.android.fueller.gui.toll.ActivityTollAdd;
 
 public class ActivityEntryAdd extends Activity {
-	
+
+	Button buttonRefuel;
+	Button buttonTyres;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_entry_add);
+
+		attachGuiObjects();
+		styleGuiObjects();
+	}
+
+	private void attachGuiObjects() {
+		buttonRefuel = (Button) findViewById(R.id.activity_entry_add_button_refuel);
+		buttonTyres = (Button) findViewById(R.id.activity_entry_add_button_tyres_change);
+	}
+
+	private void styleGuiObjects() {
+		int drawableColor = 0xFFFFFFFF;
+		buttonRefuel.getCompoundDrawables()[0].setTint(drawableColor);
+		buttonTyres.getCompoundDrawables()[0].setTint(drawableColor);
 	}
 	
 	public void onClick(View view) {
